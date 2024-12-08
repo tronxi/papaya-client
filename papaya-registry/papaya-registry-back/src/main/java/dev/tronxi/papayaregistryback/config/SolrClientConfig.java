d702e750927f9c466892d5088fa4f331c01b102c
tronxi <sergio.gsanchez97@gmail.com>
2024-12-08 13:43:59 +0100

    add solr configuration

--- papaya-registry/papaya-registry-back/src/main/java/dev/tronxi/papayaregistryback/config/SolrClientConfig.java
+++ papaya-registry/papaya-registry-back/src/main/java/dev/tronxi/papayaregistryback/config/SolrClientConfig.java
@@ -2,15 +2,21 @@ package dev.tronxi.papayaregistryback.config;
 
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SolrClientConfig {
-    private static final String SOLR_URL = "http://localhost:8983/solr";
+
+    @Value("${registry.solr.host}")
+    private String host;
+
+    @Value("${registry.solr.core}")
+    private String core;
 
     @Bean
     public SolrClient createSolrClient() {
-        return new HttpSolrClient.Builder(SOLR_URL + "/gettingstarted").build();
+        return new HttpSolrClient.Builder(host + core).build();
     }
 }
