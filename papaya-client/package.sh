8d3e413a4722b36cafaa35554efb0e2cbbec1ff6
tronxi <sergio.gsanchez97@gmail.com>
2024-12-14 16:53:59 +0100

    add icon.png

--- papaya-client/package.sh
+++ papaya-client/package.sh
@@ -1,16 +1,27 @@
- mvn clean install
+#!/bin/bash
 
- if [ -d "output" ]; then
-   rm -rf output
- fi
+mvn clean install
 
+if [ -d "output" ]; then
+  rm -rf output
+fi
 
- jpackage \
-    --input target \
-    --name "Papaya" \
-    --main-jar papaya-client-0.0.1-SNAPSHOT.jar \
-    --main-class org.springframework.boot.loader.launch.JarLauncher \
-    --type app-image \
-    --dest output \
-    --icon icons/icon.icns \
-    --verbose
+OS=$(uname)
+
+if [ "$OS" == "Darwin" ]; then
+  ICON="icons/icon.icns"
+  echo "Detected macOS. Using icon: $ICON"
+elif [ "$OS" == "Linux" ]; then
+  ICON="icons/icon.png"
+  echo "Detected Linux. Using icon: $ICON"
+fi
+
+jpackage \
+  --input target \
+  --name "Papaya" \
+  --main-jar papaya-client-0.0.1-SNAPSHOT.jar \
+  --main-class org.springframework.boot.loader.launch.JarLauncher \
+  --type app-image \
+  --dest output \
+  --icon "$ICON" \
+  --verbose
